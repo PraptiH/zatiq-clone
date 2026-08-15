ca56212d7fe42a71014b4cfa503db05d25f3c115
PraptiH <praptiantara1892@gmail.com>
2026-08-15 14:21:57 +0600

    Adjust team section responsive slider layout

--- Components/sections/CompanySection.tsx
+++ Components/sections/CompanySection.tsx
@@ -4,7 +4,7 @@ import TeamSection from "./TeamSection";
 
 export default function CompanySection() {
     return (
-        <div className="bg-blue-50 px-30 py-20 space-y-20">
+        <div className="bg-blue-50 px-4 sm:px-6 md:px-10 lg:px-16 xl:px-30 py-20 space-y-20">
             <AppDownloadSection />
 
             <FAQSection/>
--- Components/sections/TeamSection.tsx
+++ Components/sections/TeamSection.tsx
@@ -12,8 +12,8 @@ export default function TeamSection() {
     return (
         <div>
             <div className="text-center space-y-3 pb-10">
-                <h1 className="font-semibold text-5xl">The Team Powering Zatiq</h1>
-                <p className="text-[#959595] text-sm">The Visionaries Behind Zatiq, Shaping the Future of e-commerce</p>
+                <h1 className="font-semibold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">The Team Powering Zatiq</h1>
+                <p className="text-[#959595] text-sm sm:text-base md:text-lg lg:text-xl">The Visionaries Behind Zatiq, Shaping the Future of e-commerce</p>
             </div>
 
             <div>
@@ -26,11 +26,75 @@ export default function TeamSection() {
                     }}
                     speed={500}
                     allowTouchMove={false}
-                    spaceBetween={24}
+
                     breakpoints={{
-                        320: { slidesPerView: 1 },
-                        640: { slidesPerView: 3 },
-                        1024: { slidesPerView: 4.2 },
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 12,
+                        },
+
+                        425: {
+                            slidesPerView: 1.4,
+                            spaceBetween: 16,
+                        },
+
+                        // Small tablet
+                        480: {
+                            slidesPerView: 1.7,
+                            spaceBetween: 16,
+                        },
+
+                        576: {
+                            slidesPerView: 2,
+                            spaceBetween: 18,
+                        },
+
+                        640: {
+                            slidesPerView: 2.25,
+                            spaceBetween: 20,
+                        },
+
+                        768: {
+                            slidesPerView: 2.6,
+                            spaceBetween: 20,
+                        },
+
+                        900: {
+                            slidesPerView: 3,
+                            spaceBetween: 22,
+                        },
+                        1024: {
+                            slidesPerView: 3.2,
+                            spaceBetween: 24,
+                        },
+
+                        1152: {
+                            slidesPerView: 3.5,
+                            spaceBetween: 24,
+                        },
+
+
+                        1536: {
+                            slidesPerView: 3.7,
+                            spaceBetween: 24,
+                        },
+
+                        // Large desktop
+                        1680: {
+                            slidesPerView: 4.4,
+                            spaceBetween: 24,
+                        },
+
+                        1800: {
+                            slidesPerView: 4.6,
+                            spaceBetween: 24,
+                        },
+
+                        1920: {
+                            slidesPerView: 4.8,
+                            spaceBetween: 24,
+                        },
+
                     }}
                 >
                     {members.map((member, index) => (
@@ -44,7 +108,7 @@ export default function TeamSection() {
                                     className="w-full rounded-lg aspect-square object-cover"
                                 />
 
-                                <div className="flex flex-1 flex-col gap-3 px-2 pt-3">
+                                <div className="flex flex-1 grow flex-col gap-3 px-2 pt-3">
                                     <div>
                                         <h3 className="font-semibold text-sm text-gray-900">
                                             {member.name}
